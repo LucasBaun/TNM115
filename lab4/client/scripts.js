5b24d33068a27ab967b11d855c88eaee2a5b4730
Lennetorp <kunglinus02@hotmail.se>
2024-02-13 21:04:09 +0100

    senaste

--- lab4/client/scripts.js
+++ lab4/client/scripts.js
@@ -1,20 +1,85 @@
 const serverUrl = "http://127.0.0.1:3000";
 const webPageURL = new URL(document.URL);
 
-
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("HTML DOM tree loaded, and ready for manipulation.");
+    
+    action();
+ });
+ function action() {
 
 function getAllArtists() {
+    req_arg("/artists", "application/json");
 
 }
-function getOneArtist() {
-
+function getOneArtist(artist) {
+    req_arg("/artists/" + artist, "application/json");
 }
 
 getAllArtists();
 getOneArtist();
 
-async function req_arg() {
-    const respons = await fetch(serverUrl + "search", {
-        
+
+async function req_arg(search, content) {
+    const respons = await fetch(serverUrl + search, {
+        method: "GET",        
+        headers: {
+            "Content-Type": content,            
+        },
+        body: null
     });
+
+    if(search == "/artists") {
+        if(respons.ok) {
+            respons.json().then((artistList) => {
+            for(tempName in artistList) {
+                    const blockDiv = document.createElement("div");
+                    blockDiv.id = artistList[tempName]._id;
+                    const artistNameH1 = document.createElement("h1");
+                    artistNameH1.innerText = artistList[tempName].name;
+                    const temp = artistList[tempName]._id;
+                    artistNameH1.addEventListener("click", function() { 
+                        clickedArtist(temp);
+                    });
+                    console.log(artistList[tempName].name);
+                    blockDiv.appendChild(artistNameH1);
+                    document.body.appendChild(blockDiv);
+                
+            }
+
+            });
+        }
+        else {
+            const funkar2 = document.createElement("h1");
+            funkar2.innerText = "DE FUNKAR INTE";
+            document.body.appendChild(funkar);
+            console.log("DE FUNKAR INTE");
+        }
+    } else {
+        if(respons.ok) {
+            // respons.json().
+            console.log("AYYEMEN");
+        }
+        else {
+            console.log("DE FUNKAR INTE  2");
+        }
+    }
+}
+
+function clickedArtist(artistid) {
+    console.log("clickedArtist: " + artistid);
+    getOneArtist(artistid);
+}
+
+
+
+window.addEventListener('scroll', doParallax);
+function doParallax(){
+   var positionY = window.pageYOffset/2;
+   document.body.style.backgroundPosition = "0 -" + positionY + "px";
+}
+/*
+
+*/
+
 }
--- lab4/server/app.js
+++ lab4/server/app.js
@@ -19,9 +19,16 @@ const server = http.createServer((req, res) => {
     const requestUrl = new URL(serverUrl + req.url);
     const pathComponents = requestUrl.pathname.split("/");
 
-    if (req.method == "GET") {
-        switch (pathComponents[1]) {
-            case "något":
+    if (req.method == "GET") { //GET request
+        switch (pathComponents[1]) { //If the first path component is "artists"
+            case "artists":
+                if (pathComponents.length > 2) { //if there is more then 2 in path (artists/artistName)
+                    performDbOperation(res, pathComponents[2]) //Request to database artistName
+                    console.log("artistName");
+                } else {
+                    performDbOperation(res, "all_artists") //Request to database all artists
+                    console.log("all_artists");
+                }
 
                 break;
         }
@@ -32,7 +39,36 @@ const server = http.createServer((req, res) => {
 
 });
 
+async function performDbOperation(res, search) {
+    await dbClient.connect(); //connect to database
+    const db = dbClient.db(dbName); //select database
+    const dbCollection = db.collection(dbCollectionName); //select collection
+    if (search == "all_artists") {
+        const filterQuery = {}; //empty filter
+        const sortQuery = { name: 1 }; //sort alphabet
+        const projectionQuery = {_id: 1, name: 1}; //project only name
+        const findResult = await dbCollection.find(filterQuery).sort(sortQuery).project(projectionQuery).toArray(); ////find all artists and sort by name       
+        sendResponse(res, 200, "application/json", JSON.stringify(findResult));
+        
+    } else if (res != null && search != null) {   
+        console.log("search: " + search); 
+        let num = Number(search); //convert to number (id is number in database
+        const filterQuery = { _id: num }; //filter for artist name
+        const findResult = await dbCollection.find(filterQuery).toArray();//find artist        
+        sendResponse(res, 200, "application/json", JSON.stringify(findResult));
+    } else {
+        console.log("No response object");
+    }
+    
 
+}
+performDbOperation() 
+    .catch(console.error) //catch errors
+    .finally(() => { //close database connection
+    dbClient.close() //close database connection
+});
+
+//responds
 function sendResponse(res, statusCode, contentType, data) {
     res.statusCode = statusCode;
     if(contentType != null) res.setHeader("Content-Type", contentType);
