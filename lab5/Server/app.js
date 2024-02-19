ee8dd7a7b394d01ba853c5a29e481e4088ed4157
Lennetorp <kunglinus02@hotmail.se>
2024-02-19 19:30:31 +0100

    smått o gott

--- lab5/Server/app.js
+++ lab5/Server/app.js
@@ -23,12 +23,15 @@ const server = http.createServer((req, res) => {
         switch (pathComponents[1]) { //If the first path component is "artists"
             case "artists":
                 if (pathComponents.length > 2) { //if there is more then 2 in path (artists/artistName)
-                    performDbOperation(res, pathComponents[2]) //Request to database artistName
+                    performDbOperation(res, pathComponents[2], null) //Request to database artistName
                     console.log("artistName");
                 } else {
-                    performDbOperation(res, "all_artists") //Request to database all artists
+                    performDbOperation(res, "all_artists", null) //Request to database all artists
                     console.log("all_artists");
                 }
+                break;
+            case "search":
+                performDbOperation(res, null, pathComponents[2]);
                 break;
         }
 
@@ -38,45 +41,56 @@ const server = http.createServer((req, res) => {
 
 });
 
-async function performDbOperation(res, search) {
+async function performDbOperation(res, search, text) {
     await dbClient.connect(); //connect to database
     const db = dbClient.db(dbName); //select database
     const dbCollection = db.collection(dbCollectionName); //select collection
-    if (search == "all_artists") {
-        const filterQuery = {}; //empty filter
-        const sortQuery = { name: 1 }; //sort alphabet
-        const projectionQuery = {_id: 1, name: 1}; //project only name
-        const findResult = await dbCollection.find(filterQuery).sort(sortQuery).project(projectionQuery).toArray(); ////find all artists and sort by name       
-        sendResponse(res, 200, "application/json", JSON.stringify(findResult));
-        
-    } else if (res != null && search != null) {        
-        let num = Number(search); //convert to number (id is number in database
-        // const filterQuery = { _id: num}; //filter for artist name
-        // const projectionQuery = {_id: 1, name: { $ne: null }, realname: { $ne: null }};
-        
-        // const findResult = await dbCollection.find(filterQuery).project(projectionQuery).toArray();//find artist   
-        const findResult = await dbCollection.aggregate([
-            { $match: { _id: num } },
-            {
-              $project: {
-                _id: 1,
-                name: { $cond: { if: { $ne: ["$name", null] }, then: "$name", else: "$$REMOVE" } },
-                realname: { $cond: { if: { $ne: ["$realname", null] }, then: "$realname", else: "$$REMOVE" } },
-                discogsUrl: { $cond: { if: { $ne: ["$discogsUrl", null] }, then: "$discogsUrl", else: "$$REMOVE" } },                
-                description: { $cond: { if: { $ne: ["$description", null] }, then: "$description", else: "$$REMOVE" } },
-                nameVariations: { $cond: { if: { $ne: ["$nameVariations", null] }, then: "$nameVariations", else: "$$REMOVE" } },
-                // memberInGroups: { $cond: { if: { $ne: [{ $size: "$memberInGroups"}, 0 ]}, then: "$memberInGroups", else: "$$REMOVE" } }
-                aliases: { $cond: { if: { $ne: ["$aliases", null] }, then: "$aliases", else: "$$REMOVE" } },
-                memberInGroups: { $cond: { if: { $ne: ["$memberInGroups", null] }, then: "$memberInGroups", else: "$$REMOVE" } },
-                referenceUrls: { $cond: { if: { $ne: ["$referenceUrls", null] }, then: "$referenceUrls", else: "$$REMOVE" } }
-              }
-            }
-          ]).toArray();
-          console.log(findResult[0].name);          
-        
-        sendResponse(res, 200, "application/json", JSON.stringify(findResult));
+    if (text == null) {
+        if (search == "all_artists") {
+            const filterQuery = {}; //empty filter
+            const sortQuery = { name: 1 }; //sort alphabet
+            const projectionQuery = {_id: 1, name: 1}; //project only name
+            const findResult = await dbCollection.find(filterQuery).sort(sortQuery).project(projectionQuery).toArray(); ////find all artists and sort by name       
+            sendResponse(res, 200, "application/json", JSON.stringify(findResult));
+            
+        } else if (res != null && search != null) {            
+            let num = Number(search); //convert to number (id is number in database
+            // const filterQuery = { _id: num}; //filter for artist name
+            // const projectionQuery = {_id: 1, name: { $ne: null }, realname: { $ne: null }};
+            
+            // const findResult = await dbCollection.find(filterQuery).project(projectionQuery).toArray();//find artist   
+            const findResult = await dbCollection.aggregate([
+                { $match: { _id: num } },
+                {
+                $project: {
+                    _id: 1,
+                    name: { $cond: { if: { $ne: ["$name", null] }, then: "$name", else: "$$REMOVE" } },
+                    realname: { $cond: { if: { $ne: ["$realname", null] }, then: "$realname", else: "$$REMOVE" } },
+                    discogsUrl: { $cond: { if: { $ne: ["$discogsUrl", null] }, then: "$discogsUrl", else: "$$REMOVE" } },                
+                    description: { $cond: { if: { $ne: ["$description", null] }, then: "$description", else: "$$REMOVE" } },
+                    nameVariations: { $cond: { if: { $ne: ["$nameVariations", null] }, then: "$nameVariations", else: "$$REMOVE" } },
+                    // memberInGroups: { $cond: { if: { $ne: [{ $size: "$memberInGroups"}, 0 ]}, then: "$memberInGroups", else: "$$REMOVE" } }
+                    aliases: { $cond: { if: { $ne: ["$aliases", null] }, then: "$aliases", else: "$$REMOVE" } },
+                    memberInGroups: { $cond: { if: { $ne: ["$memberInGroups", null] }, then: "$memberInGroups", else: "$$REMOVE" } },
+                    referenceUrls: { $cond: { if: { $ne: ["$referenceUrls", null] }, then: "$referenceUrls", else: "$$REMOVE" } }
+                }
+                }
+            ]).toArray();
+            console.log(findResult[0].name);          
+            
+            sendResponse(res, 200, "application/json", JSON.stringify(findResult));
     } else {
         console.log("No response object");
+    }
+    
+    } else {
+        const indexResult = await dbCollection.createIndex({name: "text", description: "text", realname: "text"});        
+        filterQuery = {$text: {$search: text}};
+        const sortQuery = {name: 1};
+        const projectionQuery = {_id: 1};
+        const findResult = await dbCollection.find(filterQuery).sort(sortQuery).project(projectionQuery).toArray();
+        console.log(findResult);
+        sendResponse(res, 200, "application/json", JSON.stringify(findResult));
     }
     
 
--- lab5/client/scripts.js
+++ lab5/client/scripts.js
@@ -12,14 +12,12 @@ function getAllArtists() {
     req_arg("/artists", "application/json");
 
 }
-function getOneArtist(artist) {
-    if(document.getElementById(artist._id)){
-        console.log("hej, den finns");
-    }
+function getOneArtist(artist) {   
     req_arg("/artists/" + artist, "application/json");
 }
-
-
+function getSpecifikSearch(searchTemp) {
+    req_arg("/search/" + searchTemp, "application/json");
+}
 getAllArtists();
 
 
@@ -59,92 +57,97 @@ async function req_arg(search, content) {
             console.log("DE FUNKAR INTE");
         }
     } else {
-        if(respons.ok) {
-            respons.json().then((artistInfo) => {
-                // artistInfo = JSON.parse(artistInfo);
-                document.artistDiv = document.getElementById(artistInfo[0]._id);
-                document.infoDiv = document.createElement("div"); 
-                document.infoDiv.id = "infoDiv";
-                // for(tempName in artistInfo[0]) {};
-                const name = document.createElement("h2");
-                name.innerHTML = "<span>" + artistInfo[0].name + "</span>";
-                document.infoDiv.appendChild(name);
-                if (artistInfo[0].realname != null) {
-                    const realName = document.createElement("h2");
-                    realName.innerHTML = "<span>Real name: </span>" + "<br>" + artistInfo[0].realname;
-                    document.infoDiv.appendChild(realName);
-                }
-                if (artistInfo[0].description != null) {
-                    const description = document.createElement("p");
-                    description.innerHTML = "<span>Description: </span>" + "<br>" + artistInfo[0].description;
-                    document.infoDiv.appendChild(description);
-                }
-                if (artistInfo[0].nameVariations != null) {
-                    const nameVariations = document.createElement("p");
-                    nameVariations.innerHTML = "<span>Name variations: </span>";
-                    for (temp in artistInfo[0].nameVariations) {
-                        nameVariations.innerHTML += "<br>" + artistInfo[0].nameVariations[temp];
-                    }
+        const component = search.split("/");
+        console.log(component);
+        if (component[1] == "artists") {
+            if(respons.ok) {
+                respons.json().then((artistInfo) => {
+                    // artistInfo = JSON.parse(artistInfo);
+                    document.artistDiv = document.getElementById(artistInfo[0]._id);
+                    document.infoDiv = document.createElement("div"); 
+                    document.infoDiv.id = "infoDiv";
+                    // for(tempName in artistInfo[0]) {};
+                    const name = document.createElement("h2");
+                    name.innerHTML = "<span>" + artistInfo[0].name + "</span>";
+                    document.infoDiv.appendChild(name);                
                    
-                    document.infoDiv.appendChild(nameVariations);
-                }
-                if (artistInfo[0].aliases != null) {
-                    const aliases = document.createElement("p");
-                    aliases.innerHTML = "<span>Aliases: </span>";
-                    for (temp in artistInfo[0].aliases) {
-                        aliases.innerHTML += "<br>" + JSON.stringify(artistInfo[0].aliases[temp]);
+                    if (artistInfo[0].realname != null) {
+                        const realName = document.createElement("h2");
+                        realName.innerHTML = "<span>Real name: </span>" + "<br>" + artistInfo[0].realname;
+                        document.infoDiv.appendChild(realName);
                     }
-                    document.infoDiv.appendChild(aliases);
-                }
-                if (artistInfo[0].memberInGroups != null) {
-                    const memberInGroups = document.createElement("p");
-                    memberInGroups.innerHTML = "<span>Member in groups: </span>" + "<br>" + artistInfo[0].memberInGroups;
-                    document.infoDiv.appendChild(memberInGroups);
-                }
-                if (artistInfo[0].referenceUrls != null) {
-                    const referenceUrls = document.createElement("p");
-                    referenceUrls.innerHTML = "<span>Reference urls: </span>";
-                    for (temp in artistInfo[0].referenceUrls) { 
-                        const tempa = document.createElement("a"); //Skapar en a tagg
-                        tempa.href = artistInfo[0].referenceUrls[temp]; //lägger till länk i den
-                        tempa.target = '_blank'; //öppnar länken i ny flik
-                        tempa.innerText = artistInfo[0].referenceUrls[temp]; //lägger till text i den
-                        referenceUrls.appendChild(document.createElement("br"));
-                        referenceUrls.appendChild(tempa);
-                        //referenceUrls.innerHTML +="<br>" + tempa; //lägger till text i den
-                        console.log(artistInfo[0].referenceUrls[temp]);
+                    if (artistInfo[0].description != null) {
+                        const description = document.createElement("p");
+                        description.innerHTML = "<span>Description: </span>" + "<br>" + artistInfo[0].description;
+                        document.infoDiv.appendChild(description);
                     }
-                    document.infoDiv.appendChild(referenceUrls);
+                    if (artistInfo[0].nameVariations != null) {
+                        const nameVariations = document.createElement("p");
+                        nameVariations.innerHTML = "<span>Name variations: </span>";
+                        for (temp in artistInfo[0].nameVariations) {
+                            nameVariations.innerHTML += "<br>" + artistInfo[0].nameVariations[temp];
+                        }
+                    
+                        document.infoDiv.appendChild(nameVariations);
+                    }
+                    if (artistInfo[0].aliases != null) {
+                        const aliases = document.createElement("p");
+                        aliases.innerHTML = "<span>Aliases: </span>";
+                        for (temp in artistInfo[0].aliases) {
+                            aliases.innerHTML += "<br>" + JSON.stringify(artistInfo[0].aliases[temp]);
+                        }
+                        document.infoDiv.appendChild(aliases);
+                    }
+                    if (artistInfo[0].memberInGroups != null) {
+                        const memberInGroups = document.createElement("p");
+                        memberInGroups.innerHTML = "<span>Member in groups: </span>" + "<br>" + artistInfo[0].memberInGroups;
+                        document.infoDiv.appendChild(memberInGroups);
+                    }
+                    if (artistInfo[0].referenceUrls != null) {
+                        const referenceUrls = document.createElement("p");
+                        referenceUrls.innerHTML = "<span>Reference urls: </span>";
+                        for (temp in artistInfo[0].referenceUrls) { 
+                            const tempa = document.createElement("a"); //Skapar en a tagg
+                            tempa.href = artistInfo[0].referenceUrls[temp]; //lägger till länk i den
+                            tempa.target = '_blank'; //öppnar länken i ny flik
+                            tempa.innerText = artistInfo[0].referenceUrls[temp]; //lägger till text i den
+                            referenceUrls.appendChild(document.createElement("br"));
+                            referenceUrls.appendChild(tempa);
+                            //referenceUrls.innerHTML +="<br>" + tempa; //lägger till text i den
+                            console.log(artistInfo[0].referenceUrls[temp]);
+                        }
+                        document.infoDiv.appendChild(referenceUrls);
+                    }
+                    if (artistInfo[0].discogsUrl != null) {
+                        const discogsUrl = document.createElement("p");
+                        const temp = document.createElement("a");
+                        temp.href = artistInfo[0].discogsUrl;
+                        temp.target = '_blank'; //öppnar länken i ny flik
+                        temp.innerText = artistInfo[0].discogsUrl;
+                        discogsUrl.innerHTML = "<span>Discogs url: </span>";
+                        discogsUrl.appendChild(document.createElement("br"));
+                        discogsUrl.appendChild(temp);
+                        document.infoDiv.appendChild(discogsUrl);
+                    }
+                    document.artistDiv.appendChild(document.infoDiv);      
+
+                });
+            }
+    } else {
+        if (respons.ok) {
+            respons.json().then((artistsFound) => {                
+                if (artistsFound.length == 0) {
+                    console.log("Tommm");
+                } else {
+                    for (pass = 0; pass < artistsFound.length; ++pass) {
+                        getOneArtist(artistsFound[pass]._id);
+                    }
                 }
-                if (artistInfo[0].discogsUrl != null) {
-                    const discogsUrl = document.createElement("p");
-                    const temp = document.createElement("a");
-                    temp.href = artistInfo[0].discogsUrl;
-                    temp.target = '_blank'; //öppnar länken i ny flik
-                    temp.innerText = artistInfo[0].discogsUrl;
-                    discogsUrl.innerHTML = "<span>Discogs url: </span>";
-                    discogsUrl.appendChild(document.createElement("br"));
-                    discogsUrl.appendChild(temp);
-                    document.infoDiv.appendChild(discogsUrl);
-                }
-                document.artistDiv.appendChild(document.infoDiv);
-                const infoArg = [
-                    "_id",
-                    "discogsUrl",
-                    "name",
-                    "realname",
-                    "description",
-                    "nameVariations",
-                    "aliases",
-                    "memberInGroups",
-                    "referenceUrls"
-                ];        
 
             });
         }
-        else {
-            console.log("DE FUNKAR INTE  2");
-        }
+    }
+    
     }
 }
 
@@ -174,6 +177,7 @@ function searchInput() {
     searchBar.addEventListener("change", (e) => {
         if (searchBar.value != "") {
             searchTemp = searchBar.value;
+            getSpecifikSearch(searchTemp);
             console.log(searchTemp);
         }
     });
